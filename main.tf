3102425202902da32da336a4892b494201c37145
i2tsuki(kazuki.ono@mixi.co.jp) <9761807+i2tsuki@users.noreply.github.com>
2024-08-29 16:35:00 +0900

    chore: Add RDS module for i2tsuki and update AWS instance tags

--- main.tf
+++ main.tf
@@ -25,12 +25,3 @@ resource "aws_instance" "app_server" {
     Project = "Handson"
   }
 }
-
-module "rds-i2tsuki" {
-  source  = "app.terraform.io/MIXI/rds-i2tsuki/aws"
-  version = "1.0.0"
-  # insert required variables here
-  db_name = "main"
-  db_username = "root"
-  db_password = "toor"
-}
